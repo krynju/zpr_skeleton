d9ebbe92d548ff662ff32210064a76859bb10011
Krystian Gulinski <gulinski.krystian@gmail.com>
2019-02-27 10:34:48 +0100

    add boost

---
+++ README.md
@@ -0,0 +1,9 @@
+# zpr project placeholder
+
+- builds on vs
+- builds on msys2/g++ mingw64 in clion
+- builds on wsl gnu toolchain
+
+# todo
+
+- specify one boost version
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::cout << "hey, type something:";
+    std::for_each(
+            in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
     return 0;
 }
