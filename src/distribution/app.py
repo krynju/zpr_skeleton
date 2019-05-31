4c1cefb52dff2ce6c8df20e995e7afd354d67e13
Krystian Gulinski <gulinski.krystian@gmail.com>
2019-05-31 21:58:40 +0200

    add report handling

--- src/distribution/app.py
+++ src/distribution/app.py
@@ -1,30 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file, Blueprint, url_for
 from flask_cors import CORS
+from src.distribution.workspace_helpers import *
+
 import json
 from os import getcwd
 from os import listdir
 from os.path import isfile, join
+import os
 import pandas as pd
+
+prepare_workspace()
 
 app = Flask(__name__, template_folder="static", static_url_path='')
 CORS(app)
 
+reports = Blueprint(__name__,
+                    'reports',
+                    root_path=join(getcwd()),
+                    template_folder=join(getcwd(), workspace_dir_name),
+                    static_folder=join(workspace_dir_name, 'static'),
+                    static_url_path='/report_static')
 
-class File:
-    def __init__(self, filename):
-        self.filename = filename
+app.register_blueprint(reports)
 
-        if filename[-4:] == '.csv':
-            self.status = 'csv'
-        else:
-            self.status = 'wrong_format'
-
-        if self.status == 'csv':
-            try:
-                self.columns = read_csv_columns(filename)
-            except:
-                self.status = 'error_reading_columns'
-                self.columns = []
 
 @app.route('/')
 def root():
@@ -58,18 +56,21 @@ def csv_info():
     return {'columns': read_csv_columns(filename)}
 
 
-def read_csv_columns(filename):
-    return list(pd.read_csv(filename, nrows=1).columns)
+@app.route('/api/report', methods=['POST'])
+def report_request():
+    data = request.get_json()['data']
+    # set some var to 'working'
+    return json.dumps({'status': 'done'})
 
 
+@app.route('/api/report', methods=['GET'])
+def report_list():
+    return json.dumps(scan_for_reports())
 
 
-
-
-
-
-
-
+@app.route('/report/<report_name>')
+def see_report(report_name):
+    return render_template('%s/index.html' % report_name)
 
 
 if __name__ == '__main__':
---
+++ src/distribution/workspace_helpers.py
@@ -0,0 +1,48 @@
+import json
+from os import getcwd
+from os import listdir
+from os.path import isfile, join, isdir
+import os
+import pandas as pd
+
+workspace_dir_name = 'distribution_ws'
+
+
+def prepare_workspace():
+    if workspace_dir_name not in listdir(getcwd()):
+        os.mkdir(workspace_dir_name)
+        os.mkdir(join(workspace_dir_name, 'static'))
+    else:
+        if 'static' not in listdir(join(getcwd(), workspace_dir_name)):
+            os.mkdir(join(workspace_dir_name, 'static'))
+
+
+class File:
+    def __init__(self, filename):
+        self.filename = filename
+
+        if filename[-4:] == '.csv':
+            self.status = 'csv'
+        else:
+            self.status = 'wrong_format'
+
+        if self.status == 'csv':
+            try:
+                self.columns = read_csv_columns(filename)
+            except:
+                self.status = 'error_reading_columns'
+                self.columns = []
+
+
+def read_csv_columns(filename):
+    return list(pd.read_csv(filename, nrows=1).columns)
+
+
+def scan_for_reports():
+    ws_path = join(getcwd(), workspace_dir_name)
+
+    dirs = [f for f in listdir(ws_path) if isdir(join(ws_path, f))]
+
+    reports = list(filter(lambda x: 'index.html' in listdir(join(ws_path, x)), dirs))
+
+    return reports
